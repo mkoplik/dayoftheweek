c36e1fbd104d382462fd802da771345023253e74
mkoplik <140488550+mkoplik@users.noreply.github.com>
2023-08-13 14:42:23 +0300

    Update formDayOfTheWeek.php

--- formDayOfTheWeek.php
+++ formDayOfTheWeek.php
@@ -1,6 +1,6 @@
 <form action="result.php" method="post">
     <lable >введите год Вашего рождения</lable><br>
-	<input name="year" placeholder="Год рождения"><br><br>
+	<input name="year" placeholder="Год рожд"><br><br>
     <lable>введите месяц Вашего рождения</lable><br>
     <input name="month"><br><br>
     <lable>введите число Вашего рождения</lable><br>
